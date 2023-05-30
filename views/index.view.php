e4a1ba0516481a69bb58c4dfc14deec992733d19
Alexa Mullin <alemulli@gmail.com>
2023-05-30 09:48:18 -0400

    Update index.view.php
    Checking

--- views/index.view.php
+++ views/index.view.php
@@ -5,6 +5,7 @@
 <main>
   <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
     This website will have photos of my dogs.
+    I can make changes to it.
   </div>
 </main>
 <?php require('partials/foot.php') ?>
